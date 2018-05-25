0627f3a9aa7a11e03839ceab13d3ee61d837085b
Tong W. Ratanapan <tong.worapol@gmail.com>
2018-05-25 22:20:32 +0700

    Fixed bug: social accounts' id = undefined

--- siteBuilder.js
+++ siteBuilder.js
@@ -4,17 +4,17 @@ let contacts = {
   "linkedin": "TongWR",
   "twitter": "TongWRatanapan",
   "display": function() {
-    let contacts = $("#contacts")
+    let footerContacts = $("#contacts")
 
     let formattedLinkedIn = HTMLlinkedin.replace("%data%", contacts.linkedin)
     let formattedMedium = HTMLmedium.replace("%data%", contacts.medium)
     let formattedGitHub = HTMLgithub.replace("%data%", contacts.github)
     let formattedTwitter = HTMLtwitter.replace("%data%", contacts.twitter)
 
-    contacts.after(formattedTwitter)
-            .after(formattedLinkedIn)
-            .after(formattedGitHub)
-            .after(formattedMedium)
+    footerContacts.after(formattedTwitter)
+                  .after(formattedLinkedIn)
+                  .after(formattedGitHub)
+                  .after(formattedMedium)
   }
 }
 
