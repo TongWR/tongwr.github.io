a1784f4678587abed3860f3f7e7441992c865619
Tong W. Ratanapan <tong.worapol@gmail.com>
2018-10-11 22:07:44 +0700

    Rewrite following 'CSS in 44 minutes' book

--- helper.js
+++
@@ -1,4 +0,0 @@
-let HTMLlinkedin = '<a href="https://www.linkedin.com/in/%data%/" title="Connect with Tong on LinkedIn."><i class="fab fa-linkedin fa-2x"></i></a>'
-let HTMLmedium = '<a href="https://medium.com/@%data%" title="Read Tong\'s thoughts on Medium."><i class="fab fa-medium fa-2x"></i></a>'
-let HTMLtwitter = '<a href="https://twitter.com/%data%" title="Scroll past random shit Tong\'s retweeted."><i class="fab fa-twitter fa-2x"></i></a>'
-let HTMLgithub = '<a href="https://github.com/%data%" title="View Tong\'s dead repositories."><i class="fab fa-github fa-2x"></i></a>'
